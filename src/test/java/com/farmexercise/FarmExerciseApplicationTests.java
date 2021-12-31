815cd648b981f1b0fe39254b51b390463934f533
Janne Kähkönen <jm.kahkonen@gmail.com>
2021-12-31 17:25:20 +0200

    Java Spring projektin luonti

---
+++ src/test/java/com/farmexercise/FarmExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
