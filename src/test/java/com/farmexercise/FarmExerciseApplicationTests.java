b8a6ea6798b1320351ff399ed36760f74027b6fc
Janne Kähkönen <jm.kahkonen@gmail.com>
2022-01-15 13:40:10 +0200

    Luotu ensimmäiset aloitus testit

--- src/test/java/com/farmexercise/FarmExerciseApplicationTests.java
+++ src/test/java/com/farmexercise/FarmExerciseApplicationTests.java
@@ -1,5 +1,7 @@
 package com.farmexercise;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class FarmExerciseApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	// Testaan, että testi toimii
+	@Test
+	void aloitusTesti() {
+		assertTrue(true);
 	}
 
 }
