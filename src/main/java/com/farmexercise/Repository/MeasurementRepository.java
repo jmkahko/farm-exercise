06a62dadd2fd8d7e85a12d3a640a54ea9f60da38
Janne Kähkönen <jm.kahkonen@gmail.com>
2022-01-16 17:23:47 +0200

    Korjattu aikahakuun Instant Stringin tilalle

--- src/main/java/com/farmexercise/Repository/MeasurementRepository.java
+++ src/main/java/com/farmexercise/Repository/MeasurementRepository.java
@@ -1,5 +1,6 @@
 package com.farmexercise.Repository;
 
+import java.time.Instant;
 import java.util.List;
 
 import com.farmexercise.Model.Measurement;
@@ -9,6 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
     List<Measurement> findByLocationAndSensortype(String location, String sensortype);
     List<Measurement> findByDatetimeBetweenAndLocation(String alkuPaiva, String loppuPaiva, String location);
-    List<Measurement> findByDatetimeBetweenAndLocationAndSensortype(String alkuPaiva, String loppuPaiva, String location, String sensortype);
+    List<Measurement> findByDatetimeBetweenAndLocationAndSensortype(Instant alkuPaiva, Instant loppuPaiva, String location, String sensortype);
 
 }
